a3f440f07a56ed3c8dc07e84fc00b303655f0422
muzeef999 <shaikmuzeef9999@gmail.com>
2025-10-23 16:19:57 +0530

    cahnges happend

--- frontend/web/src/app/page.tsx
+++ frontend/web/src/app/page.tsx
@@ -1,26 +1,26 @@
 
-export  default async  function Home () {
+export  default   function Home () {
 
-    const authres = await fetch("http://localhost:4000/v1/auth") ;
-    const authData = await authres.json();
+    // const authres = await fetch("http://localhost:4000/v1/auth") ;
+    // const authData = await authres.json();
 
-    const  paymentres = await fetch("http://localhost:4000/v1/payment") ;
-    const  PaymentData = await  paymentres.json();
+    // const  paymentres = await fetch("http://localhost:4000/v1/payment") ;
+    // const  PaymentData = await  paymentres.json();
 
-    const  propertyres = await fetch("http://localhost:4000/v1/property") ;
-    const  propertyData = await  propertyres.json();
+    // const  propertyres = await fetch("http://localhost:4000/v1/property") ;
+    // const  propertyData = await  propertyres.json();
 
-    const  userres = await fetch("http://localhost:4000/v1/user") ;
-    const  userData = await  userres.json();
+    // const  userres = await fetch("http://localhost:4000/v1/user") ;
+    // const  userData = await  userres.json();
 
 
   return (
    <div>
       <h1>Welcome to Propernu</h1>
-      <p>{authData.message}</p>
+      {/* <p>{authData.message}</p>
       <p>{PaymentData.message}</p>
       <p>{propertyData.message}</p>
-      <p>{userData.message}</p>
+      <p>{userData.message}</p> */}
    </div>
   );
 }
